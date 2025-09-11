83ee3f1a8ee6e631a3d96914682548c28d34e458
XtremeDust <tance3tr@gmail.com>
2025-09-10 23:48:05 -0400

    animation modal

--- univita-app/src/components/sections/ButtonComent.tsx
+++ univita-app/src/components/sections/ButtonComent.tsx
@@ -19,59 +19,60 @@ export default function Comment() {
                 <h2 className=" lg:text-4xl text-center lg:text-justify">¿Listo Para Elevar el nivel de la Experiencia Deportiva?</h2>
                 <p className="text-[20px]  text-center lg:text-justify">Queremos que nos des tu opinión y dejes tu experiencia par que podamos seguir creiendo contigo</p>
             </div>
-            <Button onClick={()=>(handleOpenModal)} className="text-[18px] text-white lg:text-[20px] rounded-xl transition-all duration-300 hover:scale-105 bg-unimar px-4 py-3
+            <Button onClick={handleOpenModal} className="text-[18px] text-white lg:text-[20px] rounded-xl transition-all duration-300 hover:scale-105 bg-unimar px-4 py-3
             ">               
                     Dejanos Tu Comentario               
             </Button>
 
-           
-                <Modal>
-                    <ContentModal className="space-y-22 ">
-                        <div className="flex flex-col header-modal items-end justify-center">
-                            <Button variant={"btn-danger"} className="rounded-full" onClick={handleCloseModal}>X</Button>                            
-                            <div className="text-center w-full">
-                                Buzon de Comentarios
-                            </div>                            
-                        </div>
-                        {/*
-                        
-                        <div className="flex flex-nowrap w-full header-modal items-center justify-center">
-                            <div className=" w-full">
-                                Comentario
-                            </div>                            
-                            <Button variant={"btn-danger"} className="rounded-full" onClick={handleCloseModal}>X</Button>                            
-                        </div>
-                        */}
+                <Modal state={OpenModal}>
+                    {OpenModal &&(
+                        <ContentModal className="space-y-22 ">
+                            <div className="flex flex-col header-modal items-end justify-center">
+                                <Button variant={"btn-danger"} className="rounded-full" onClick={handleCloseModal}>X</Button>                            
+                                <div className="text-center w-full">
+                                    Buzon de Comentarios
+                                </div>                            
+                            </div>
+                            {/*
+                            
+                            <div className="flex flex-nowrap w-full header-modal items-center justify-center">
+                                <div className=" w-full">
+                                    Comentario
+                                </div>                            
+                                <Button variant={"btn-danger"} className="rounded-full" onClick={handleCloseModal}>X</Button>                            
+                            </div>
+                            */}
 
-                        <div className="main-modal space-y-10">
-                            <div className="Email space-y-2">
-                                <div className="input space-x-2">
-                                    <label htmlFor="correo">Email</label>
-                                    <Input className=" p-0.5 w-xs" type="text" placeholder="example.1234@unimar.edu.ve"/>
+                            <div className="main-modal space-y-10">
+                                <div className="Email space-y-2">
+                                    <div className="input space-x-2">
+                                        <label htmlFor="correo">Email</label>
+                                        <Input className=" p-0.5 w-xs" type="text" placeholder="example.1234@unimar.edu.ve"/>
+                                    </div>
+                                    <div className="flex input space-x-1 justify-center items-center">
+                                        <label className="text-[20px]">
+                                            desea compartir su informacion
+                                        </label>
+                                        <Input className=" p-0.5 size-[18px]" type="checkbox"/>
+                                    </div>
                                 </div>
-                                <div className="flex input space-x-1 justify-center items-center">
-                                    <label className="text-[20px]">
-                                        desea compartir su informacion
-                                    </label>
-                                    <Input className=" p-0.5 size-[18px]" type="checkbox"/>
+
+                                <div className="Coment space-x-2">
+                                    <label htmlFor="correo">Comentario</label>
+                                    <TextArea className="p-0.5 w-sm h-[12rem]"/>
                                 </div>
                             </div>
 
-                            <div className="Coment space-x-2">
-                                <label htmlFor="correo">Comentario</label>
-                                <TextArea className="p-0.5 w-sm h-[12rem]"/>
+                            <div className="footer-modal space-x-[28rem]">
+                                <Button variant={"btn-secondary"} onClick={handleCloseModal}>
+                                    Cerrar
+                                </Button>
+                                <Button variant={'btn-primary'}>
+                                    Enviar
+                                </Button>
                             </div>
-                        </div>
-
-                        <div className="footer-modal space-x-[28rem]">
-                            <Button variant={"btn-secondary"}>
-                                Cerrar
-                            </Button>
-                            <Button variant={'btn-primary'}>
-                                Enviar
-                            </Button>
-                        </div>
-                    </ContentModal>
+                        </ContentModal>
+                    )}
                 </Modal>
            
         </div>
--- univita-app/src/components/ui/modal.tsx
+++ univita-app/src/components/ui/modal.tsx
@@ -1,16 +1,18 @@
 "use client"
 import React, {useState} from "react"
 
+
 export interface ModalProps{
     children?: React.ReactNode;
-    open?: boolean;
-    onClose?: () => void;
     className?:string;
+    state?:boolean;
 }
 
-    export function Modal({children,className,open,...props}:ModalProps){               
+    export function Modal({children,className, state,...props}:ModalProps){               
         return(
-                <div className={`  fixed inset-0 place-content-center place-items-center backdrop-blur-3xl  backdrop-grayscale-75 z-30 transition-opacity duration-300 ${className}`} {...props}>
+                <div className={`fixed overflow-hidden inset-0 place-content-center place-items-center transition-all ease-in-out duration-500 z-30 
+                 ${state===true ? 'backdrop-blur-3xl backdrop-grayscale-75 max-h-screen delay-400': 'delay-100 max-h-0 backdrop-blur-xs backdrop-grayscale-75' }
+                  ${className}`} {...props}>
                     {children}
                 </div>
         )
